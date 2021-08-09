8832acf6ecdb03c1b63cb0f4c036297ec19d9cc4
DerexScript <Derex@outlook.com.br>
2021-08-09 15:33:18 -0300

    AC_2021-08-09T15:33:18

--- resources/views/auth/emailVerifiedSucess.blade.php
+++ resources/views/auth/emailVerifiedSucess.blade.php
@@ -1,14 +1,15 @@
-<!doctype html>
-<html lang="pt-BR">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{{config('app.name')}} - Email Verificado Com Sucesso</title>
-</head>
-<body>
-<h1>Email Verificado Com Sucesso!</h1>
-<a href="{{route('dashboard')}}">Dashboard</button></a>
-</body>
-</html>
+@extends('templates.base')
+
+@section('title', 'Email Verificado Com Sucesso')
+
+@section('content')
+    <div class="container mt-4">
+        <div class="row d-flex justify-content-center">
+            <div class="col-6">
+                <h2>Email Verificado Com Sucesso!</h2>
+                <a href="{{route('dashboard')}}" class="text-decoration-none text-success">Click aqui </a>para acessar a Dashboard.
+            </div>
+        </div>
+    </div>
+@endsection
+
--- resources/views/auth/verifyEmail.blade.php
+++ resources/views/auth/verifyEmail.blade.php
@@ -6,7 +6,7 @@
     <div class="container mt-4">
         <div class="row d-flex justify-content-center">
             <div class="col-6">
-                <h2>Foi enviado um email, contendo um link de ativação para seu cadastro!</h2>
+                <h2>Foi enviado um email contendo um link de ativação, para seu cadastro!</h2>
                 <div class="text-muted">Verifique em sua caixa de <b>Spam</b> ou <b>lixo eletrônico</b></div>
                 <form action="{{route('verification.send')}}" class="d-flex justify-content-center mt-4" method="POST">
                     <button type="submit" class="btn btn-outline-primary w-50 rounded-2">Reenviar email de verificação</button>
