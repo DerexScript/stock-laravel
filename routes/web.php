e24b7bd1f4eef70efac24b92b25a87da76f45ae0
DerexScript <Derex@outlook.com.br>
2021-08-09 17:14:10 -0300

    AC_2021-08-09T17:14:10

--- routes/web.php
+++ routes/web.php
@@ -59,6 +59,7 @@ Route::fallback(function () {
 
 Route::get('/teste', function (Request $request) {
     try {
+        Redis:set('asd', '123456');
         $redis = Redis::get("asd");
         echo 'redis working'.$redis;
     } catch (\Predis\Connection\ConnectionException $e) {
