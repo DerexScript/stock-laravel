15e99f8a9bcaa6b4edada2d2aac7fe95bf0400e4
DerexScript <Derex@outlook.com.br>
2021-08-09 15:19:07 -0300

    AC_2021-08-09T15:19:07

--- resources/views/auth/verifyEmail.blade.php
+++ resources/views/auth/verifyEmail.blade.php
@@ -1,15 +1,18 @@
-<!doctype html>
-<html lang="pt-BR">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{{config('app.name')}} - Verify Email</title>
-</head>
-<body>
-<h1>Foi enviado um email contendo um link de ativação do seu cadastro!</h1>
-<form action="{{route('verification.send')}}" id="form1" method="POST" style="display: none;">@CSRF</form>
-<button onclick="document.querySelector('#form1').submit()">Reenviar email de verificação</button>
-</body>
-</html>
+@extends('templates.base')
+
+@section('title', 'Verify Email')
+
+@section('content')
+    <div class="container mt-4">
+        <div class="row d-flex justify-content-center">
+            <div class="col-6">
+                <h2>Foi enviado um email, contendo um link de ativação para seu cadastro!</h2>
+                <div class="text-muted">Verifique em sua caixa de <b>Spam</b> ou <b>lixo eletrônico</b></div>
+                <form action="{{route('verification.send')}}" class="d-flex justify-content-center mt-4" method="POST">
+                    <button type="submit" class="btn btn-outline-primary w-50 rounded-2">Reenviar email de verificação</button>
+                    @CSRF
+                </form>
+            </div>
+        </div>
+    </div>
+@endsection
